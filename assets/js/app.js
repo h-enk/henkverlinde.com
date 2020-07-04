730788ab2f2f3ac5cacda4fca36507b2efe71938
Henk Verlinde <henk@ventizo.com>
2020-07-04 11:31:45 +0200

    Update for GA

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-88189921-1');
+
 if (document.querySelector('.next-page') !== null) {
 
   /* eslint-disable */
