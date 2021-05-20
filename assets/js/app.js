3b8e44333ddf45c7aebed984222a9a02964d9ef8
Henk Verlinde <henk@ventizo.com>
2021-05-20 11:31:34 +0200

    fix: html intialize masonry

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars, no-undef */
-var elem = document.querySelector('.grid');
-var msnry = new Masonry( elem, {
-  horizontalOrder: true,
-  percentPosition: true,
-  transitionDuration: 0,
-});
-/* eslint-enable no-unused-vars, no-undef */
