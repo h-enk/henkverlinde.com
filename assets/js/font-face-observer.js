950c6284167a18617303e07ea13590d57d6adec5
Henk Verlinde <henk@ventizo.com>
2021-06-11 12:09:00 +0200

    test: add font face observer

---
+++ assets/js/font-face-observer.js
@@ -0,0 +1,8 @@
+// Source: https://github.com/bramstein/fontfaceobserver
+var FontFaceObserver = require('fontfaceobserver');
+
+var font = new FontFaceObserver('Inter var');
+
+font.load().then(function () {
+  document.documentElement.className += " fonts-loaded";
+});
