49dd22e0e0bd589766f211de67256ae328a250ac
Henk Verlinde <henk@ventizo.com>
2021-06-11 15:42:51 +0200

    test: remove font face observer

--- assets/js/font-face-observer.js
+++
@@ -1,8 +0,0 @@
-// Source: https://github.com/bramstein/fontfaceobserver
-var FontFaceObserver = require('fontfaceobserver');
-
-var font = new FontFaceObserver('Inter var');
-
-font.load().then(function () {
-  document.documentElement.className += ' fonts-loaded';
-});
--- config/postcss.config.js
+++ config/postcss.config.js
@@ -11,7 +11,6 @@ module.exports = {
         './content/**/*.md',
       ],
       safelist: [
-        'fonts-loaded',
         'lazyloaded',
         'table',
         'thead',
