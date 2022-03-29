2e7f495056f29bfecd4e344c32e5554ecab4f149
Henk Verlinde <henk@ventizo.com>
2022-03-29 10:17:03 +0200

    feat: add custom scss + js files

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+/** Import modules */
+import './node_modules/@ventizo/core/assets/js/modules/email.js';
+import './node_modules/@ventizo/core/assets/js/modules/alert.js';
+
+/** Import custom scripts */
+import './assets/js/modules/custom.js';
