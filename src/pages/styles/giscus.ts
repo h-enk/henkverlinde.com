4d907392fb001a6c20bc46546230feffad245b16
Henk Verlinde <henk@henkverlinde.com>
2025-07-28 10:08:35 +0200

    Fix giscus console errors

--- src/pages/styles/giscus.ts
+++ src/pages/styles/giscus.ts
@@ -8,6 +8,20 @@ export async function ALL() {
   res.headers.set('Content-Type', 'text/css');
   res.headers.set('Access-Control-Allow-Origin', '*');
   res.headers.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
+  res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Accept, Origin, User-Agent');
+  res.headers.set('Access-Control-Max-Age', '86400');
+  res.headers.set('Cache-Control', 'public, max-age=31536000, immutable');
 
+  return res;
+}
+
+export async function OPTIONS() {
+  const res = new Response(null, { status: 204 });
+  
+  res.headers.set('Access-Control-Allow-Origin', '*');
+  res.headers.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
+  res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Accept, Origin, User-Agent');
+  res.headers.set('Access-Control-Max-Age', '86400');
+  
   return res;
 }
